de577f10763d783ece50cee7f7e761879fd91d80
ParalaxRus <paralax_rus@hotmail.com>
2025-05-22 19:14:49 -0700

    singleton update

--- main.cc
+++ main.cc
@@ -1,18 +1,48 @@
+#include <vector>
+#include <thread>
+
 #include "singleton/singleton.h"
 
+namespace {
+
+namespace pt = patterns::singleton;
+
+void createSingleton(std::vector<pt::Singleton*>& instances, std::mutex& mtx) {
+    std::lock_guard<std::mutex> lock(mtx);
+    instances.push_back(pt::Singleton::get());
+}
+
+void singletonTest() {
+    std::mutex mtx;
+    std::vector<pt::Singleton*> instances = {};
+    std::vector<std::thread> threads;
+
+    constexpr int num = 10;
+    for (int i = 0; i < num; ++i) {
+        threads.emplace_back(createSingleton, std::ref(instances), std::ref(mtx));
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    if (instances.size() != num) {
+        throw std::runtime_error("Singleton pattern failed");
+    }
+
+    for (size_t i = 1; i < instances.size(); ++i) {
+        if (instances[i] != instances[i-1]) {
+            throw std::runtime_error("Singleton pattern failed");    
+        }
+    }
+}
+
+}
+
 int main() {
+    singletonTest();
 
-    const auto* single1 = patterns::singleton::Singleton::get();
-    if (single1 == nullptr) {
-        throw std::runtime_error("Singleton pattern failed");
-    }
-    single1->hi();
-
-    const auto* single2 = patterns::singleton::Singleton::get();
-    if (single1 != single2) {
-        throw std::runtime_error("Singleton pattern failed");
-    }
-    single2->hi();
+    std::cout << "unit tests pass" << std::endl;
 
     return 0;
 }
